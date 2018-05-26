c363e016430034b4089c3812ed815409ed2dd75c
Maokeita <35873907+Maokeita@users.noreply.github.com>
2018-05-26 09:03:13 -0600

    arreglos sabado
    eston son los arreglos que hicimos el sabado 26 reunidos antes de presentar

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ const Neurona = new brain.NeuralNetwork();
 
 Neurona.train([{
     input: {
-      s1: 0,
-      s2: 0,
-      s3: 0,
-      s4: 0
+      s1: 0.80,
+      s2: 0.80,
+      s3: 0.80,
+      s4: 0.80
     },
     output: {
       sf0: 1,
@@ -33,10 +33,10 @@ Neurona.train([{
   },
   {
     input: {
-      s1: 0.471,
-      s2: 0.125,
-      s3: 0.83,
-      s4: 0.152
+      s1: 0.70,
+      s2: 0.70,
+      s3: 0.70,
+      s4: 0.80
     },
     output: {
       sf0: 0,
@@ -48,9 +48,9 @@ Neurona.train([{
   {
     input: {
       s1: 0.733,
-      s2: 0.121,
-      s3: 0.79,
-      s4: 0.146
+      s2: 0.74,
+      s3: 0.80,
+      s4: 0.72
     },
     output: {
       sf0: 0,
@@ -63,8 +63,8 @@ Neurona.train([{
     input: {
       s1: 0.331,
       s2: 0.230,
-      s3: 0.40,
-      s4: 0.136
+      s3: 0.83,
+      s4: 0.90
     },
     output: {
       sf0: 0,
@@ -76,7 +76,7 @@ Neurona.train([{
   {
     input: {
       s1: 0.733,
-      s2: 0.121,
+      s2: 0.81,
       s3: 0.79,
       s4: 0.146
     },
@@ -86,8 +86,163 @@ Neurona.train([{
       sf2: 0,
       sf3: 0
     }
+  },
+  {
+    input: {
+      s1: 0.733,
+      s2: 0.81,
+      s3: 0.79,
+      s4: 0.86
+    },
+    output: {
+      sf0: 0,
+      sf1: 1,
+      sf2: 0,
+      sf3: 1
+    }
+  },
+  {
+    input: {
+      s1: 0.733,
+      s2: 0.81,
+      s3: 0.98,
+      s4: 0.146
+    },
+    output: {
+      sf0: 0,
+      sf1: 1,
+      sf2: 1,
+      sf3: 0
+    }
+  },
+  {
+    input: {
+      s1: 0.43,
+      s2: 0.81,
+      s3: 0.86,
+      s4: 0.99
+    },
+    output: {
+      sf0: 0,
+      sf1: 1,
+      sf2: 1,
+      sf3: 1
+    }
+  },
+  {
+    input: {
+      s1: 0.833,
+      s2: 0.25,
+      s3: 0.49,
+      s4: 0.65
+    },
+    output: {
+      sf0: 1,
+      sf1: 0,
+      sf2: 0,
+      sf3: 0
+    }
+  },
+  {
+    input: {
+      s1: 0.833,
+      s2: 0.31,
+      s3: 0.79,
+      s4: 0.86
+    },
+    output: {
+      sf0: 1,
+      sf1: 0,
+      sf2: 0,
+      sf3: 1
+    }
+  },
+  {
+    input: {
+      s1: 0.933,
+      s2: 0.71,
+      s3: 0.99,
+      s4: 0.146
+    },
+    output: {
+      sf0: 1,
+      sf1: 0,
+      sf2: 1,
+      sf3: 0
+    }
+  },
+  {
+    input: {
+      s1: 0.833,
+      s2: 0.61,
+      s3: 0.84,
+      s4: 0.83
+    },
+    output: {
+      sf0: 1,
+      sf1: 0,
+      sf2: 1,
+      sf3: 1
+    }
+  },
+  {
+    input: {
+      s1: 0.86,
+      s2: 0.81,
+      s3: 0.77,
+      s4: 0.646
+    },
+    output: {
+      sf0: 1,
+      sf1: 1,
+      sf2: 0,
+      sf3: 0
+    }
+  },
+  {
+    input: {
+      s1: 0.86,
+      s2: 0.81,
+      s3: 0.67,
+      s4: 0.846
+    },
+    output: {
+      sf0: 1,
+      sf1: 1,
+      sf2: 0,
+      sf3: 1
+    }
+  },
+  {
+    input: {
+      s1: 0.81,
+      s2: 0.88,
+      s3: 0.97,
+      s4: 0.546
+    },
+    output: {
+      sf0: 1,
+      sf1: 1,
+      sf2: 1,
+      sf3: 0
+    }
+  },
+  {
+    input: {
+      s1: 0.733,
+      s2: 0.55,
+      s3: 0.79,
+      s4: 0.146
+    },
+    output: {
+      sf0: 0,
+      sf1: 0,
+      sf2: 0,
+      sf3: 0
+    }
   }
 ]);
+
 port.on('data', function(data) {
   //console.log("Valor 1 " + Sensor1 + " Valor 2 " + Sensor2 + " Valor 3 " + Sensor3 + " Valor 4 " + Sensor4);
   for (var i = 0; i < data.length; i++) {
@@ -134,48 +289,107 @@ port.on('data', function(data) {
           Sensor3Tmp = Sensor3Tmp * 10 + data[i];
         }
         break;
-        case 4:
-          if (data[i] == 105) {
-            Sensor4 = Sensor4Tmp;
-            Estado = 0;
-            var respuesta = Neurona.run({
-              input: {
-                s1: Sensor1,
-                s2: Sensor2,
-                s3: Sensor3,
-                s4: Sensor4
-              }
-            }); // [0.987]
-            console.log("la respuesta es " + respuesta.sf0);
-            console.log("la respuesta es " + respuesta.sf1);
-            console.log("la respuesta es " + respuesta.sf2);
-            console.log("la respuesta es " + respuesta.sf3);
-            if (respuesta.sf0 > 0.75) {
-              port.write("A");
-              console.log("la letra es a");
-            } else {
-              port.write("B");
+      case 4:
+        if (data[i] == 105) {
+          Sensor4 = Sensor4Tmp;
+          Estado = 0;
+          var respuesta = Neurona.run({
+            input: {
+              s1: Sensor1,
+              s2: Sensor2,
+              s3: Sensor3,
+              s4: Sensor4
             }
-            if (respuesta.sf1 > 0.75) {
-              port.write("C");
-            } else {
-              port.write("D");
-            }
-            if (respuesta.sf2 > 0.75) {
-              port.write("E");
-            } else {
-              port.write("F");
-            }
-            if (respuesta.sf3 > 0.75) {
-              port.write("G");
-            } else {
-              port.write("H");
-            }
-            //console.log("El Valor es:" + Sensor1);
-          } else {
-            Sensor4Tmp = Sensor4Tmp * 10 + data[i];
+          }); // [0.987]
+          console.log("la respuesta1 es " + respuesta.sf0);
+          console.log("la respuesta2 es " + respuesta.sf1);
+          console.log("la respuesta3 es " + respuesta.sf2);
+          console.log("la respuesta4 es " + respuesta.sf3);
+          //1111
+          if (respuesta.sf0 > 0.75 && respuesta.sf1 > 0.75 && respuesta.sf2 > 0.75 && respuesta.sf3 > 0.75) {
+            port.write("A");
+            console.log("la letra es a");
           }
-          break;
-      }
+          //1110
+          else if (respuesta.sf0 > 0.75 && respuesta.sf1 > 0.75 && respuesta.sf2 > 0.75 && respuesta.sf3 < 0.75) {
+            port.write("B");
+            console.log("la letra es b");
+          }
+          //1101
+          else if (respuesta.sf0 > 0.75 && respuesta.sf1 > 0.75 && respuesta.sf2 < 0.75 && respuesta.sf3 > 0.75) {
+            port.write("C");
+            console.log("la letra es c");
+          }
+          //1100
+          else if (respuesta.sf0 > 0.75 && respuesta.sf1 > 0.75 && respuesta.sf2 < 0.75 && respuesta.sf3 < 0.75) {
+            port.write("D");
+            console.log("la letra es d");
+          }
+          //1011
+          else if (respuesta.sf0 > 0.75 && respuesta.sf1 < 0.75 && respuesta.sf2 > 0.75 && respuesta.sf3 > 0.75) {
+            port.write("E");
+            console.log("la letra es e");
+          }
+          //1010
+          else if (respuesta.sf0 > 0.75 && respuesta.sf1 < 0.75 && respuesta.sf2 > 0.75 && respuesta.sf3 < 0.75) {
+            port.write("F");
+            console.log("la letra es f");
+          }
+          //1001
+          else if (respuesta.sf0 > 0.75 && respuesta.sf1 < 0.75 && respuesta.sf2 < 0.75 && respuesta.sf3 > 0.75) {
+            port.write("G");
+            console.log("la letra es g");
+          }
+          //1000
+          else if (respuesta.sf0 > 0.75 && respuesta.sf1 < 0.75 && respuesta.sf2 < 0.75 && respuesta.sf3 < 0.75) {
+            port.write("H");
+            console.log("la letra es h");
+          }
+          //0111
+          else if (respuesta.sf0 < 0.75 && respuesta.sf1 > 0.75 && respuesta.sf2 > 0.75 && respuesta.sf3 > 0.75) {
+            port.write("I");
+            console.log("la letra es i");
+          }
+          //0110
+          else if (respuesta.sf0 < 0.75 && respuesta.sf1 > 0.75 && respuesta.sf2 > 0.75 && respuesta.sf3 < 0.75) {
+            port.write("J");
+            console.log("la letra es j");
+          }
+          //0101
+          else if (respuesta.sf0 < 0.75 && respuesta.sf1 > 0.75 && respuesta.sf2 < 0.75 && respuesta.sf3 > 0.75) {
+            port.write("K");
+            console.log("la letra es k");
+          }
+          //0100
+          else if (respuesta.sf0 < 0.75 && respuesta.sf1 > 0.75 && respuesta.sf2 < 0.75 && respuesta.sf3 < 0.75) {
+            port.write("L");
+            console.log("la letra es l");
+          }
+          //0011
+          else if (respuesta.sf0 < 0.75 && respuesta.sf1 < 0.75 && respuesta.sf2 > 0.75 && respuesta.sf3 > 0.75) {
+            port.write("M");
+            console.log("la letra es m");
+          }
+          //0010
+          else if (respuesta.sf0 < 0.75 && respuesta.sf1 < 0.75 && respuesta.sf2 > 0.75 && respuesta.sf3 < 0.75) {
+            port.write("N");
+            console.log("la letra es n");
+          }
+          //0001
+          else if (respuesta.sf0 < 0.75 && respuesta.sf1 < 0.75 && respuesta.sf2 < 0.75 && respuesta.sf3 > 0.75) {
+            port.write("O");
+            console.log("la letra es o");
+          }
+          //0000
+          else if (respuesta.sf0 < 0.75 && respuesta.sf1 < 0.75 && respuesta.sf2 < 0.75 && respuesta.sf3 < 0.75) {
+            port.write("P");
+            console.log("la letra es p");
+          }
+          //console.log("El Valor es:" + Sensor1);
+        } else {
+          Sensor4Tmp = Sensor4Tmp * 10 + data[i];
+        }
+        break;
     }
-  });
+  }
+});
--- sensorneurona.ino
+++ sensorneurona.ino
@@ -1,11 +1,17 @@
-int Sensor1 = A2;
-int Sensor2 = A0;
+int Sensor1 = A0;
+int Sensor2 = A2;
 int Sensor3 = A3;
 int Sensor4 = A4;
-int salida = 2;
+int salida1 = 2;
+int salida2 = 3;
+int salida3 = 4;
+int salida4 = 5;
 char recibir;
 void setup() {
-  pinMode(salida, OUTPUT);
+  pinMode(salida1, OUTPUT);
+  pinMode(salida2, OUTPUT);
+  pinMode(salida3, OUTPUT);
+  pinMode(salida4, OUTPUT);
   pinMode(Sensor1, INPUT);
   pinMode(Sensor2, INPUT);
   pinMode(Sensor3, INPUT);
@@ -19,7 +25,7 @@ void loop() {
   int Valor2 = analogRead(Sensor2);
   int Valor3 = analogRead(Sensor3);
   int Valor4 = analogRead(Sensor4);
-  //Sensor 1 1234 4 3 2 1 
+  //Sensor 1 1234 4 3 2 1
   for (int i = 0; i <= 3; i++) {
     Residuo[i] = Valor1 % 10;
     Valor1 = Valor1 / 10;
@@ -41,10 +47,9 @@ void loop() {
   for (int i = 3; i >= 0; i--) {
     Serial.write(Residuo[i]);
   }
-  }
   Serial.write('e');
 
-  //Sensor 3 1234 4 3 2 1 
+  //Sensor 3 1234 4 3 2 1
   for (int i = 0; i <= 3; i++) {
     Residuo[i] = Valor3 % 10;
     Valor3 = Valor3 / 10;
@@ -56,7 +61,7 @@ void loop() {
   }
   Serial.write('g');
 
-//Sensor 4 1234 4 3 2 1 
+//Sensor 4 1234 4 3 2 1
   for (int i = 0; i <= 3; i++) {
     Residuo[i] = Valor4 % 10;
     Valor4 = Valor4 / 10;
@@ -71,12 +76,101 @@ void loop() {
    if (Serial.available() ) {
     char letra = Serial.read();
     if (letra == 'A') {
-      digitalWrite(salida, LOW);
+      digitalWrite(salida1, LOW);
+      digitalWrite(salida2, LOW);
+      digitalWrite(salida3, LOW);
+      digitalWrite(salida4, LOW);
     }
-    //Encender el boton de la alarma
     else if (letra == 'B') {
-      digitalWrite(salida, HIGH);
-      }
+      digitalWrite(salida1, LOW);
+      digitalWrite(salida2, LOW);
+      digitalWrite(salida3, LOW);
+      digitalWrite(salida4, HIGH);
+    }
+    else if (letra == 'C') {
+      digitalWrite(salida1, LOW);
+      digitalWrite(salida2, LOW);
+      digitalWrite(salida3, HIGH);
+      digitalWrite(salida4, LOW);
+    }
+    else if (letra == 'D') {
+      digitalWrite(salida1, LOW);
+      digitalWrite(salida2, LOW);
+      digitalWrite(salida3, HIGH);
+      digitalWrite(salida4, HIGH);
+    }
+    else if (letra == 'E') {
+      digitalWrite(salida1, LOW);
+      digitalWrite(salida2, HIGH);
+      digitalWrite(salida3, LOW);
+      digitalWrite(salida4, LOW);
+    }
+    else if (letra == 'F') {
+      digitalWrite(salida1, LOW);
+      digitalWrite(salida2, HIGH);
+      digitalWrite(salida3, LOW);
+      digitalWrite(salida4, HIGH);
+    }
+    else if (letra == 'G') {
+      digitalWrite(salida1, LOW);
+      digitalWrite(salida2, HIGH);
+      digitalWrite(salida3, HIGH);
+      digitalWrite(salida4, LOW);
+    }
+    else if (letra == 'H') {
+      digitalWrite(salida1, LOW);
+      digitalWrite(salida2, HIGH);
+      digitalWrite(salida3, HIGH);
+      digitalWrite(salida4, HIGH);
+    }
+    else if (letra == 'I') {
+      digitalWrite(salida1, HIGH);
+      digitalWrite(salida2, LOW);
+      digitalWrite(salida3, LOW);
+      digitalWrite(salida4, LOW);
+    }
+    else if (letra == 'J') {
+      digitalWrite(salida1, HIGH);
+      digitalWrite(salida2, LOW);
+      digitalWrite(salida3, LOW);
+      digitalWrite(salida4, HIGH);
+    }
+    else if (letra == 'K') {
+      digitalWrite(salida1, HIGH);
+      digitalWrite(salida2, LOW);
+      digitalWrite(salida3, HIGH);
+      digitalWrite(salida4, LOW);
+    }
+    else if (letra == 'L') {
+      digitalWrite(salida1, HIGH);
+      digitalWrite(salida2, LOW);
+      digitalWrite(salida3, HIGH);
+      digitalWrite(salida4, HIGH);
+    }
+    else if (letra == 'M') {
+      digitalWrite(salida1, HIGH);
+      digitalWrite(salida2, HIGH);
+      digitalWrite(salida3, LOW);
+      digitalWrite(salida4, LOW);
+    }
+    else if (letra == 'N') {
+      digitalWrite(salida1, HIGH);
+      digitalWrite(salida2, HIGH);
+      digitalWrite(salida3, LOW);
+      digitalWrite(salida4, HIGH);
+    }
+    else if (letra == 'O') {
+      digitalWrite(salida1, HIGH);
+      digitalWrite(salida2, HIGH);
+      digitalWrite(salida3, HIGH);
+      digitalWrite(salida4, LOW);
+    }
+    else if (letra == 'P') {
+      digitalWrite(salida1, HIGH);
+      digitalWrite(salida2, HIGH);
+      digitalWrite(salida3, HIGH);
+      digitalWrite(salida4, HIGH);
+    }
 }
   //Serial.println();
   delay(1000);
